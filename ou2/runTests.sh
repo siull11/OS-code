cafd6f1133037af783afca7e8c3342c29632598a
Simon Ullerstam <simon@ullerstam.se>
2023-02-15 15:46:35 +0100

    msg

--- ou2/runTests.sh
+++ ou2/runTests.sh
@@ -4,19 +4,21 @@ mpiCC test1.cpp my_mpi.cpp -o test1
 mpiCC test2.cpp my_mpi.cpp -o test2
 
 seed=0
-max_procs=4
-max_procs_factorial=$(seq -s "*" 1 $max_procs |bc)
-max_n=200 #10000000
+max_procs=64
+start_n=1000
+# max_procs_factorial=$(seq -s "*" 1 $max_procs |bc)
+max_n=1000000
 step_n=2
 
 for t in test1 test2
 do
     echo "Running $t"
-    for ((i=1; i<=$max_procs; i++))
+    for ((i=1; i<=$max_procs; i*=2)) # i++)) 
     do 
         echo "with $i processes"
         echo "n, total, time" > "output/$t-$i.csv"
-        for ((j=$max_procs_factorial; j<=$max_n; j*=$step_n))
+        n=$(( $start_n / $i ))
+        for ((j=$n*$i; j<=$max_n; j*=$step_n))
         do
             mpirun -np $i ./$t $j $seed >> "output/$t-$i.csv"
         done
